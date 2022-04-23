00218185bbec820ac6c69111ceecfa1928091034
JMine97 <52027965+JMine97@users.noreply.github.com>
2022-04-23 13:46:26 +0900

    :wrench: config: config setting

---
+++ backend/src/main/java/a204/ssayeon/SsayeonApplication.java
@@ -0,0 +1,13 @@
+package a204.ssayeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsayeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsayeonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/a204/ssayeon/SsayeonApplicationTests.java
@@ -0,0 +1,13 @@
+package a204.ssayeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsayeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
