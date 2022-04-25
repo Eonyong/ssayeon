7862555741708eec6f2d0c28338dd93326c4f8b1
Eonyong <unjoo94@naver.com>
2022-04-25 14:28:55 +0900

    :sparkles: feat: sidebar layout add

--- frontend/src/components/common/Sidebar.js
+++ frontend/src/components/common/Sidebar.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import { Button, Fab, ButtonGroup, Divider, Typography } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import styled from  'styled-components';
 
 const Side = styled.div`
   display: flex;
   border-right: 1px solid #e0e0e0;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
   width: 10%;
 `
@@ -14,12 +15,41 @@ const Side = styled.div`
 function SideBar() {
   return(
     <Side>
-      <button>공지사항</button>
-      {/* dropdown */}
-      <button>게시판</button>
-      <button>싸피 놀이터</button>
-      <button>모임</button>
-      <button>공지사항</button>
+      <Divider />
+      <Button>공지사항</Button>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography>게시판</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <ButtonGroup fullWidth
+            orientation="vertical" variant="text"
+          >
+            <Fab variant="extended" sx={{ m:1 }}>자유 게시판</Fab>
+            <Fab variant="extended">질문 게시판</Fab>
+            <Fab variant="extended">꿀팁 게시판</Fab>
+          </ButtonGroup>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography>싸피 놀이터</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <ButtonGroup fullWidth
+            orientation="vertical" variant="text"
+          >
+            <Fab variant="extended">밸런스 게임</Fab>
+            <Fab variant="extended">선호도 조사</Fab>
+          </ButtonGroup>
+        </AccordionDetails>
+      </Accordion>
+      <Button>모임</Button>
+      <Button>공지사항</Button>
     </Side>
   );
 };
