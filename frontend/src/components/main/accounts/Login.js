2f456de9821638d4e75c666dedf76db54e5d0818
Eonyong <unjoo94@naver.com>
2022-04-27 15:38:02 +0900

    :lipstick: design: Sign Up 페이지 레이아웃 수정

--- frontend/src/components/main/accounts/Login.js
+++ frontend/src/components/main/accounts/Login.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Box, Button, Checkbox, FormControlLabel, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Checkbox, Container, FormControlLabel, Grid, TextField, Typography } from "@mui/material";
 
 export default function Login () {
 
@@ -13,47 +13,48 @@ export default function Login () {
   };
 
   return (
-    <Box
-      sx={{
-        marginTop: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '50%'
-      }}
-    >
-      <Grid container direction='column'>
-        <Typography variant="h5" sx={{ mb: 2 }}>Login</Typography>
-        <Typography>Sign in with your data that you entered during your registration.</Typography>
-        <Box
-          noValidate container component='form'
-          sx={{ my: 5 }}
-        >
-          <TextField
-            id='email' name="email" autoComplete="email"
-            type='email' placeholder="Email@email.com" label='Email'
-            fullWidth margin='normal' required autoFocus sx={{ mb: 1 }}
-          />
-          <TextField
-            id="password" name="password" autoComplete='current-password'
-            type='password' placeholder="패스워드를 입력해주세요" label='Password'
-            fullWidth sx={{ mt: 1 }}
-          />
-          <FormControlLabel
-            control={<Checkbox value='remember' color="primary" />}
-            label='Keep me logged in'
-          />
-          <Button
-            type="sumbmit" sx={{ py: 1 }}
-            fullWidth variant="contained"
+    <Container>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Grid container direction='column'>
+          <Typography variant="h5" sx={{ mb: 2 }}>Login</Typography>
+          <Typography>Sign in with your data that you entered during your registration.</Typography>
+          <Box
+            noValidate container component='form'
+            sx={{ my: 5 }} onSubmit={ handleSubmit }
           >
-            Sign In
-          </Button>
-        </Box>
-        <Link to='#' variant="body2">
-          Forgot password?
-        </Link>
-      </Grid>
-    </Box>
+            <TextField
+              id='email' name="email" autoComplete="email"
+              type='email' placeholder="Email@email.com" label='Email'
+              fullWidth margin='normal' required autoFocus sx={{ mb: 1 }}
+            />
+            <TextField
+              id="password" name="password" autoComplete='current-password'
+              type='password' placeholder="패스워드를 입력해주세요" label='Password'
+              fullWidth sx={{ mt: 1 }}
+            />
+            <FormControlLabel
+              control={<Checkbox value='remember' color="primary" />}
+              label='Keep me logged in'
+            />
+            <Button
+              type="sumbmit" sx={{ py: 1, backgroundColor: '#4B7BF5' }}
+              fullWidth variant="contained"
+            >
+              Sign In
+            </Button>
+          </Box>
+          <Link to='#' variant="body2" color="#4B7BF5">
+            Forgot password?
+          </Link>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
--- frontend/src/components/main/accounts/Signup.js
+++ frontend/src/components/main/accounts/Signup.js
@@ -1,28 +1,148 @@
-import { Box, Button, FormControl, FormLabel, Grid, Input, InputLabel, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, TextField, Typography, Container } from "@mui/material";
 
 export default function Signup() {
-  return (
-    <Box component='form' sx={{ width: '60%' }}>
-      <Grid container direction='column'>
-        <Typography variant="h1" sx={{ mb: 2 }}>Signup</Typography>
-        <Box container component='form'
-          sx={{ flexDirection: 'column', my: 5 }}
-        >
-          <FormControl>
-            <FormLabel htmlFor="name">이름</FormLabel>
-            <Input
-              type='text' placeholder="홍길동"
-              fullWidth sx={{ mb: 1 }} id='name'
-            />
-          </FormControl>
-          <TextField
-            type='number' placeholder="0000000" label='싸피 학번'
-            fullWidth sx={{ my: 1 }}
-          />
-          <Button fullWidth variant="contained" sx={{ py: 1 }}>교육생 인증</Button>
-        </Box>
 
-      </Grid>
-    </Box>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Grid container direction='column'>
+          <Typography variant="h5" sx={{ mb: 1 }}>Sign Up</Typography>
+
+          {/* 이름, 학번 입력 Field */}
+          <Box
+            noValidate container component='form'
+            sx={{ mt: 3, mb: 5 }} onSubmit={ handleSubmit }
+          >
+
+            {/* 이름 입력 Field */}
+            <TextField
+              id='name' name="name" autoComplete="name" margin='normal'
+              type='text' placeholder="이름" label='name'
+              fullWidth required autoFocus sx={{ mb: 1 }}
+            />
+
+            {/* 학번 입력 Field */}
+            <TextField
+              id="studentId" name="studentId" autoComplete='current-studentId'
+              type='number' placeholder="0000000" label='학번'
+              fullWidth required sx={{ mt: 1 }}
+            />
+
+            {/* 인증하기 Button Field */}
+            <Button
+              type="sumbmit" sx={{ py: 1, mt: 2, backgroundColor: '#4B7BF5' }}
+              fullWidth variant="contained"
+            >
+              인증하기
+            </Button>
+          </Box>
+        </Grid>
+      </Box>
+      {/* 두번째 입력 Form Start */}
+      <Box
+        sx={{
+          marginTop: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Grid container direction='column'>
+          <Box
+            noValidate container component='form' onSubmit={ handleSubmit }
+          >
+            {/* 닉네임, 중복확인 Field Start */}
+            <Grid
+              container spacing={2}
+              sx={{ alignItems: 'center' }}
+            >
+              <Grid item xs={9}>
+                <TextField
+                  id='nickname' name="nickname" autoComplete="nickname" margin='normal'
+                  type='text' placeholder="닉네임" label='닉네임'
+                  fullWidth required
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  type="submit" variant='text'
+                >
+                  중복 확인
+                </Button>
+              </Grid>
+            </Grid>
+
+            {/* Email 작성 Field */}
+            <Grid
+              container spacing={2}
+              sx={{ alignItems: 'center' }}
+            >
+              <Grid item xs={9}>
+                <TextField
+                  id='email' name="email" autoComplete="email" margin='normal'
+                  type='email' placeholder="Email@email.com" label='Email'
+                  fullWidth required sx={{ mb: 1 }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  type="submit" variant='text'
+                >
+                  중복 확인
+                </Button>
+              </Grid>
+            </Grid>
+
+            
+
+            {/* Password Form Field */}
+            <Grid
+              container spacing={2}
+              sx={{ alignItems: 'center', mt: 1, mb: 3  }}
+            >
+              <Grid item xs={12} sm={6} >
+                <TextField
+                  id="password" name="password" autoComplete='current-password'
+                  type='password' placeholder="패스워드를 입력해주세요" label='Password'
+                  fullWidth required 
+                  />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="passwordConfirm" name="passwordConfirm" autoComplete='current-passwordConfirm'
+                  type='password' placeholder="패스워드를 입력해주세요" label='Password 확인'
+                  fullWidth required
+                  />
+              </Grid>
+            </Grid>
+
+            {/* 제출 Button Field */}
+            <Button
+              type="sumbmit" sx={{ py: 1, backgroundColor: '#4B7BF5' }}
+              fullWidth variant="contained"
+            >
+              Sign Up
+            </Button>
+
+          </Box>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
