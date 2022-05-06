2950f8fb08f5fdbbb8498c1d74c68a76ee68f6ff
IMS <gudwnsrh@gmail.com>
2022-05-06 15:22:31 +0900

    :art: fix: PaginationResponseBody 추가 AdvancedResponseBody 수정

--- backend/src/main/java/a204/ssayeon/common/model/response/AdvancedResponseBody.java
+++ backend/src/main/java/a204/ssayeon/common/model/response/AdvancedResponseBody.java
@@ -1,32 +1,31 @@
 package a204.ssayeon.common.model.response;
 
 import a204.ssayeon.common.model.enums.Status;
-import a204.ssayeon.db.entity.Pagination;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class
-AdvancedResponseBody<T> extends BaseResponseBody {
+public class AdvancedResponseBody<T> extends BaseResponseBody {
     T data;
 
     public static <T> AdvancedResponseBody<T> of(Status status, T data) {
         return (AdvancedResponseBody<T>) AdvancedResponseBody.
-                builder().
+                advancedBuilder().
                 status(status).
                 data(data).
                 build();
     }
     public static <T> AdvancedResponseBody<T> of(Status status) {
         return (AdvancedResponseBody<T>) AdvancedResponseBody.
-                builder().
+                advancedBuilder().
                 status(status).
                 build();
     }
 
-    @Builder
+    @Builder(builderMethodName = "advancedBuilder")
     public AdvancedResponseBody(Status status, T data) {
         super(status);
         this.data = data;
