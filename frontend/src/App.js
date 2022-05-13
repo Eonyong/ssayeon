61c2642ad14dbdce2a5cf0913c9762452597ad13
강동석 <kds2048@naver.com>
2022-05-13 14:02:58 +0900

    :sparkles: feat: modify App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,6 @@ import { Divider, Grid } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-<<<<<<< HEAD
-import SideBar from './components/common/Sidebar'
-import Signup from './components/main/accounts/Signup';
-import Login from './components/main/accounts/Login';
-import NoticeList from './components/main/boards/notice/NoticeList';
-import NoticeDetail from './components/main/boards/notice/NoticeDetail';
-import NewNotice from './components/main/boards/notice/NewNotice';
-import Profile from './components/main/accounts/Profile';
-import Main from './components/common/Main';
-import { useDispatch } from 'react-redux';
-import { userProfile } from './user/auth';
-import BalanceList from "./components/main/balance/BalanceList";
-import BalanceContent from "./components/main/balance/BalanceContent";
-=======
 import SideBar from "./components/common/Sidebar";
 import Signup from "./components/main/accounts/Signup";
 import Login from "./components/main/accounts/Login";
@@ -30,15 +16,13 @@ import { useEffect } from "react";
 import PreferenceList from "./components/main/preference/PreferenceList";
 import RegisterPreference from "./components/main/preference/RegisterPreference";
 import PreferenceDetail from "./components/main/preference/PreferenceDetail";
->>>>>>> feature/frontend/preference
 
 function App() {
   const dispatch = useDispatch();
   if (!localStorage.getItem("token")) {
     localStorage.clear();
-  } else{
+  } else {
     dispatch(userProfile());
-
   }
 
   return (
@@ -51,14 +35,14 @@ function App() {
           <Divider orientation="vertical" flexItem />
           <Grid item sx={{ textAlign: "-webkit-center" }} xs={9}>
             <Routes>
-              <Route path='/' element={ <Main /> } />
-              <Route path='auth/login' element={ <Login /> } />
-              <Route path='auth/join' element={ <Signup /> } />
-              <Route path='/profile' element={ <Profile /> } />
-              <Route path='/boards/notice/new' element={ <NewNotice /> } />
-              <Route path='/boards/notice' element={ <NoticeList />}/>
-              <Route path='/balance/list' element={ <BalanceList />}/>
-              <Route path='/balance/:id' element={ <BalanceContent />}/>
+              <Route path="/" element={<Main />} />
+              <Route path="auth/login" element={<Login />} />
+              <Route path="auth/join" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/boards/notice/new" element={<NewNotice />} />
+              <Route path="/boards/notice" element={<NoticeList />} />
+              <Route path="/balance/list" element={<BalanceList />} />
+              <Route path="/balance/:id" element={<BalanceContent />} />
               {/* id값으로 rerouting */}
               <Route path="/boards/notice/detail" element={<NoticeDetail />} />
               <Route path="/preference" element={<PreferenceList />} />
